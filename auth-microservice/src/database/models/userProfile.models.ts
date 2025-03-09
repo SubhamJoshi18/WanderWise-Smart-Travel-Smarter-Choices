c28561638c2928a7ef88e79649747f416bc84cd0
SubhamJoshi18 <shubhamrajjoshi69@gmail.com>
2025-03-09 22:21:36 +0545

    add the setup for the project

---
+++ auth-microservice/src/database/models/userProfile.models.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserProfileSchema = new mongoose.Schema({
+    isActive : {
+        type : Boolean,
+        default : true
+    },
+    image :  {
+        type : String,
+        default :''
+    },
+
+    SecondaryEmail : {
+        type : String,
+        default : ''
+    },
+
+    isUser : {
+        type : Boolean,
+        default : true
+    },
+
+    location : {
+        type : String,
+        default : ''
+    },
+    user : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : [true,'User Must be Required']
+    }
+}, {
+    timestamps : true
+})
+
+
+const UserProfile = mongoose.model('UserProfile',UserProfileSchema)
+export default UserProfile
---
+++ auth-microservice/src/routes/server.routes.ts
@@ -0,0 +1,10 @@
+import {Application} from 'express'
+import healthRouter from './health.routes'
+
+const serverRouter = (app : Application) => {
+    app.use('/api',[healthRouter])
+}
+
+export {
+    serverRouter
+}
