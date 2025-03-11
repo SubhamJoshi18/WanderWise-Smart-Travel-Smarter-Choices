9ca8b82d42846a5bce4615c7449f7cda010ba35b
sSuUjJaAlL <sujal.20207015@nami.edu.np>
2025-03-11 21:33:10 +0545

    deactivation-activation

--- auth-microservice/src/routes/auth.routes.ts
+++ auth-microservice/src/routes/auth.routes.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Router } from 'express';
 import {
   loginController,
@@ -10,3 +11,18 @@ authRouter.post('/auth/signup', signupController);
 authRouter.post('/auth/login', loginController);
 
 export default authRouter
+=======
+import {Router} from "express";
+import { loginController, signupController } from "../controller/auth.controller";
+
+const authRouter=Router();
+
+authRouter.post('/auth/signup',signupController);
+authRouter.post('/auth/login',loginController);
+authRouter.post('/auth/deactivate',deactivateController)
+
+
+export default authRouter
+
+
+>>>>>>> 57c8667 (deactivation-activation)
