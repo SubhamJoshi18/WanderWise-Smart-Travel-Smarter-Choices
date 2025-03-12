808ec5e1e1fcef443f783f0d0756ee1069b47fd4
Subodh2084 <subodhnepal10@gmail.com>
2025-03-12 21:21:43 +0545

    sujal

--- auth-microservice/src/controller/auth.controller.ts
+++ auth-microservice/src/controller/auth.controller.ts
@@ -4,38 +4,38 @@ import { forgetPasswordSchema, resetPasswordSchema } from "../validation/auth.va
 import {forgetPasswordService, checkResetToken, resetPasswordServices } from "../services/auth.services";
 import { IforgetPasswordBody } from "../interface/auth.interface";
 
-async function forgetPasswordController(req:Request, res:Response, next:NextFunction) {
+export async function forgetPasswordController(req:Request, res:Response, next:NextFunction) {
     try {
         const content = req.body;
         const parseBody = await forgetPasswordSchema.parseAsync(content);
         const apiResponse=await forgetPasswordService(parseBody as IforgetPasswordBody)
-        return res.status(200).json({
+         res.status(200).json({
             response:apiResponse
         })}
     catch (err: any) {
         next(err)
     }
 }
-async function checkResetLink(req:Request,res:Response,next:NextFunction) {
+export async function checkResetLink(req:Request,res:Response,next:NextFunction) {
             try{
                 const tokenId = req.params.tokenId
                 const apiResposne = await checkResetToken(tokenId)
                 const contentMessage = `The Provided Token is Valid`
                 
-                return res.status(200).json({
+                 res.status(200).json({
                     response: apiResposne
                 }) }catch(err){
                 next(err)
         }
 }
-async function resetPassword(req:Request,res:Response,next:NextFunction){
+export async function resetPassword(req: Request, res: Response, next: NextFunction) {
         try{
             const tokenId = req.params.tokenId
             const userId = req.params.userId
             const content = await resetPasswordSchema.parseAsync(req.body)
             const apiResposne = await resetPasswordServices(tokenId,userId,content)
             const contentMessage = `The Provided Token is Valid`
-            return res.status(200).json({
+             res.status(200).json({
                 response:apiResposne
             })
         }catch(err){
@@ -67,5 +67,3 @@ async function resetPassword(req:Request,res:Response,next:NextFunction){
 
 
 
-
-export default forgetPasswordController
--- auth-microservice/src/routes/auth.routes.ts
+++ auth-microservice/src/routes/auth.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import forgetPasswordController from "../controller/auth.controller";
+import {forgetPasswordController,  checkResetLink } from "../controller/auth.controller";
+import { any } from "zod";
+import { updatePassword } from "../repository/auth.repository";
 const authRouter = Router();
-authRouter.post("/auth/forgetPassword", forgetPasswordController as any)
+authRouter.post("/auth/forgetPassword", forgetPasswordController)
+authRouter.get("/auth/check-link/:u", checkResetLink)
+authRouter.post("/auth/reset-password/:tokenId/:userId",updatePassword)
 export default authRouter;
